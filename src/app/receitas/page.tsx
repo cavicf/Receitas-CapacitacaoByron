f86388c42848b91e3d007ea541efa346a1d04f65
Camily <cavicfina@gmail.com>
2025-06-25 11:27:41 -0300

    feat: estrutura da pagina receitas

---
+++ src/app/receitas/page.tsx
@@ -0,0 +1,12 @@
+export default function ReceitasPage(){
+    return (
+        <main className="flex-grow py-8">
+            <div className="container mx-auto">
+                <h1 className="text-3xl font-bold">Todas as Receitas</h1>
+                <div>
+                    {/* Array de card de receitas */}
+                </div>
+            </div>
+        </main>
+    );
+}
