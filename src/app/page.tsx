ecfd8567b26996556bddb946455cc07a25457100
Camily <cavicfina@gmail.com>
2025-06-25 09:29:53 -0300

    feat: estrutura da seção Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex-grow">
-      <h1>Configuração inicial</h1>
+      <div className="container mx-auto">
+        <section>
+          <h1>Receitas deliciosas</h1>
+          <p>Descubra receitas simples e saborosas para todas as ocasiões</p>
+          <Link href='/receitas'>Ver todas as receitas</Link>
+        </section>
+      </div>
     </main>
   );
 }
