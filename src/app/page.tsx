6a6811232b223a496906c6b64fdcb11e3c4dcb3a
Camily <cavicfina@gmail.com>
2025-06-25 09:46:21 -0300

    feat: adiciona estilos da secao Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex-grow">
-      <div className="container mx-auto">
-        <section>
-          <h1>Receitas deliciosas</h1>
-          <p>Descubra receitas simples e saborosas para todas as ocasiões</p>
-          <Link href='/receitas'>Ver todas as receitas</Link>
+        <section className="bg-orange-50 py-12">
+          <div className="flex flex-col gap-6 items-center container mx-auto">
+            <h1 className="text-5xl font-bold">Receitas deliciosas</h1>
+            <p className="text-xl">Descubra receitas simples e saborosas para todas as ocasiões</p>
+            <Link className="bg-orange-500 text-white font-bold rounded-lg px-3 py-2 hover:scale-105 transition-all" href='/receitas'>Ver todas as receitas</Link>
+          </div>
         </section>
-      </div>
     </main>
   );
 }
