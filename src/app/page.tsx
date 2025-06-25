5de6938c66fa3636c893aded0c7d7b908d0e9362
Camily <cavicfina@gmail.com>
2025-06-25 09:56:50 -0300

    feat: estrutura da secao receitas em destaque

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { ChevronRight } from "lucide-react";
 import Link from "next/link";
 
 export default function Home() {
@@ -8,6 +9,17 @@ export default function Home() {
             <h1 className="text-5xl font-bold">Receitas deliciosas</h1>
             <p className="text-xl">Descubra receitas simples e saborosas para todas as ocasiões</p>
             <Link className="bg-orange-500 text-white font-bold rounded-lg px-3 py-2 hover:scale-105 transition-all" href='/receitas'>Ver todas as receitas</Link>
+          </div>
+        </section>
+
+        <section>
+          <div className="container mx-auto">
+            <h2>Receitas em destaque</h2>
+            {/*cards de receita*/}
+            <Link href=''>
+              Ver todas as receitas
+              <ChevronRight/>
+            </Link>
           </div>
         </section>
     </main>
