b763159f13c1dc13752d2c7d0c04446cffd010d2
Camily <cavicfina@gmail.com>
2025-06-26 14:51:45 -0300

    feat: adicionando responsividade aos cards de receita

--- src/app/receitas/page.tsx
+++ src/app/receitas/page.tsx
@@ -6,7 +6,7 @@ export default function ReceitasPage(){
         <main className="flex-grow py-8">
             <div className="container mx-auto">
                 <h1 className="text-3xl font-bold text-center lg:text-left">Todas as Receitas</h1>
-                <div className="grid grid-rows-3 lg:grid-cols-3 gap-8 mt-8">
+                <div className="grid grid-rows-3 justify-items-center lg:grid-cols-3 gap-8 mt-8">
                     {recipes.map((recipe)=> (
                         <RecipeCard key={recipe.id} recipe={recipe}/>
                     ))}
