10ac5667a916ae564af616a116a2c3800b9568a8
Camily <cavicfina@gmail.com>
2025-07-02 23:00:12 -0300

    feat: previne comportamento de navegacao nos botoes de editar e deletar

--- src/components/RecipeCard/index.tsx
+++ src/components/RecipeCard/index.tsx
@@ -1,4 +1,5 @@
 import { Recipe } from "@/lib/data";
+import { Edit, Trash, Trash2 } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -7,6 +8,16 @@ interface RecipeCarsProps{
 }
 
 export default function RecipeCard({recipe}: RecipeCarsProps){
+    const handleEdit = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        
+    }
+
+    const handleDelete = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+    
+    }
+
     return (
         <Link href={`/receitas/${recipe.id}`}>
             <div className="border border-slate-200 rounded-xl w-3xs lg:w-full overflow-hidden shadow-sm hover:shadow-md transition-shadow">
@@ -18,9 +29,24 @@ export default function RecipeCard({recipe}: RecipeCarsProps){
                         className="object-cover"
                     />
                 </div>
-                <div className="flex flex-col p-4 gap-2">
-                    <h3 className="text-lg font-bold">{recipe.title}</h3>
-                    <p>{recipe.description}</p>
+                <div className="flex flex-col p-4 gap-6">
+                    <div className="space-y-2">
+                        <h3 className="text-lg font-bold hover:text-orange-500 transition-colors">{recipe.title}</h3>
+                        <p>{recipe.description}</p>
+                    </div>
+                    <div className="flex items-center justify-between w-full">
+                        <span className="text-sm text-gray-500 rounded-sm bg-gray-200 px-2 py-1">
+                            {recipe.category}
+                        </span>
+                        <div className="flex gap-2">
+                            <button type="button" onClick={(e) => handleEdit(e)} className="p-2 border border-gray-200 rounded-sm hover:bg-gray-200 transition-colors cursor-pointer">
+                                <Edit size={16}/>
+                            </button>
+                            <button type="button" onClick={(e) => handleDelete(e)} className="p-2 border border-gray-200 rounded-sm hover:bg-gray-200 transition-colors cursor-pointer">
+                                <Trash2 size={16}/>
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </Link>
