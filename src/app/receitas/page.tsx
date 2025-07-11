0247b86abb289cda6ebb2762bda74a64f8c654a3
Camily <cavicfina@gmail.com>
2025-07-11 17:52:46 -0300

    feat: adiciona toasts para notificar alterações

--- src/app/receitas/page.tsx
+++ src/app/receitas/page.tsx
@@ -7,6 +7,7 @@ import api from "@/lib/api";
 import type {Recipe} from "@/lib/data"
 import { Plus, Search } from "lucide-react";
 import { useEffect, useState } from "react";
+import { toast } from "sonner"
 
 export default function ReceitasPage(){
     const [isRecipeModalOpen, setIsRecipeModalOpen] = useState(false)
@@ -23,6 +24,7 @@ export default function ReceitasPage(){
                 setRecipes(response.data)
             } catch (error) {
                 console.error('Erro ao requisitar as receitas: ', error)
+                toast.error('Erro ao requisitar receitas, tente novamente mais tarde!')
             }
         }
         fetchRecipes();
@@ -46,8 +48,10 @@ export default function ReceitasPage(){
                 setIsDeleteConfirmationModalOpen(false)
                 setSelectedRecipe(undefined)
             }
+            toast.success('Receita excluida com sucesso!')
         } catch (error) {
             console.error('erro ao deletar receita', error)
+            toast.error('Erro ao deletar receita!')
         }
         
     }
@@ -74,14 +78,18 @@ export default function ReceitasPage(){
                 const response = await api.post('/recipes', recipeData)
                 const newRecipe = response.data
                 setRecipes((prev) => [...prev, newRecipe])
+                toast.success('Receita criada com sucesso!')
             }else{
                 const updateRecipe = recipeData as Recipe
                 const response = await api.put(`/recipes/${updateRecipe.id}`, updateRecipe)
                 setRecipes((prev)=> prev.map((recipe) => (recipe.id === updateRecipe.id ? response.data : recipe )))
+                toast.success('Receita editada com sucesso!')
             }
         handleCloseModal()
+        
         } catch (error) {
             console.error(`Erro ao ${modalMode==="create" ? 'create' : 'editar'} a receita`, error)
+            toast.error(`Erro ao ${modalMode==="create" ? 'create' : 'editar'} a receita`)
         }
         
     }
